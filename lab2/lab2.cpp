879d3edf8eccf22195e9d21d70d2d59ff8c9c6fd
muronyk10072001 <muronyk10072001@ukr.net>
2019-03-05 18:38:31 +0200

    add lab2.cpp

---
+++ lab2/lab2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct team
+{
+	string name;
+	int rating;
+	team()
+	{
+		rating = 0;
+	}
+};
+
+int points(...)
+
+int read(...)
+
+void print(...)
+
+void sort(...)
+
+void write(...)
+
+int main()
+{
+	team *array;
+	int size;
+
+	size = read(array);
+	print(array, size);
+
+	sort(array, size);
+	print(array, size);
+	write(array, size);
+
+	return 0;
+}
